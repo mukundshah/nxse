8fb8998fe0b1756ccbb10daedf2df65123c5f1b8
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-01-25 17:58:05 +0545

    chore: cleanup

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,16 +1,5 @@
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/devtools', '../src/module', '@nuxt/ui'],
-  serverExtension: {
-    adminSchema: './server/admin.ts',
-    drizzle: {
-      config: {
-        schema: './server/db/schema.ts',
-        driver: 'better-sqlite',
-        dbCredentials: {
-          url: './db.sqlite',
-        },
-      },
-    },
-  },
+  modules: ['@nuxt/ui', 'nxse'],
+
+  nxse: {},
 })
